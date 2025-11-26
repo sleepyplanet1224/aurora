1876d73fb63955a3e57fd315d4ce8d66887b9ead
dan travin <dtravin@optonline.net>
2025-11-26 16:00:26 +0900

    working on adding event but not finished

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,18 @@
 class EventsController < ApplicationController
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to dashboard_path, notice: "Event created successfully."
+    else
+      redirect_to dashboard_path, alert: "Failed to create event."
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :new_total_assets, :new_saved_amount, :month_id)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @event = Event.new
+    @events = Event.all.order(created_at: :desc)
   end
-
 end
