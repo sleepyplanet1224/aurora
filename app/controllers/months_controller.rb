3c8d7ee0849fe115fddc3942e04bed4a357d6201
Stevenson Davis Stevensondavis1@gmail.com <stevensondavis1@gmail.com>
2025-11-26 18:13:13 +0900

    added_a_month_new_create

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,2 +1,39 @@
 class MonthsController < ApplicationController
+  def new
+    @month = Month.new
+  end
+
+  def create
+    @month = Month.new(month_params)
+    @month.user = current_user
+
+    if @month.save
+      start_month = @month.date
+      end_month   = (start_month + 30.years).end_of_month
+
+      current = start_month
+      total_assets = @month.total_assets
+      saved_amount = @month.saved_amount
+
+      while current <= end_month
+        total_assets += saved_amount
+        current_user.months.find_or_create_by!(
+          date: current,
+          user: current_user,
+          total_assets: total_assets,
+          saved_amount: saved_amount
+        )
+        current = current.next_month
+      end
+      redirect_to dashboard_path, notice: "Month and 30 years of months created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def month_params
+    params.require(:month).permit(:total_assets, :saved_amount, :date)
+  end
 end
