2aa3e09965e7260617f88dd83ba8c83dda42961f
Douglas Berkley <douglasmberkley@gmail.com>
2025-12-04 17:24:50 +0900

    logic

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -8,8 +8,9 @@ class MonthsController < ApplicationController
     @month.user = current_user
 
     if @month.save
+      # Build 80 years of baseline months
       start_month = @month.date
-      end_month   = (start_month + 30.years).end_of_month
+      end_month   = (start_month + 80.years).end_of_month
 
       current = start_month
       total_assets = @month.total_assets
@@ -29,39 +30,47 @@ class MonthsController < ApplicationController
         current = current.next_month
       end
 
-      current_user.update(retirement_age: params[:retirement_age].to_i,
-                          monthly_expenses: params[:monthly_expenses].to_i)
-
-      # ----------------- CHANGED PART -----------------
-      # Original code:
-      # month = current_user.months.find_by(date: retirement_date.beginning_of_month)
-      # This could return nil and cause NoMethodError
-
-      retirement_date = current_user.birthday + params[:retirement_age].to_i.years
-
-      # Use find_or_create_by to ensure month exists
-      current_user.months.find_or_create_by(date: retirement_date.beginning_of_month) do |m|
-        m.user = current_user              # Associate with current user
-        m.total_assets = total_assets      # Safe default total assets
-        m.saved_amount = 0                 # No savings in retirement month
-        m.interest_rate = interest_rate    # Use last known interest rate
-      end
-      # ----------------- END CHANGED PART -----------------
-
-      current_user.update(retirement_age: params[:retirement_age].to_i,
-                          monthly_expenses: params[:monthly_expenses].to_i)
-      retirement_date = current_user.birthday + params[:retirement_age].to_i.years
-      month = current_user.months.find_by(date: retirement_date.beginning_of_month)
-
-      @event = Event.create(
-        name: "retirement",
-        month: month,
-        new_total_assets: month.total_assets,
-        new_saved_amount: -current_user.monthly_expenses
+      # Save retirement preferences on the user
+      current_user.update(
+        retirement_age: params[:retirement_age].to_i,
+        monthly_expenses: params[:monthly_expenses].to_i
       )
+
+      # Automatically determine the first possible retirement month:
+      # find the earliest month where monthly interest on total assets
+      # is greater than or equal to the desired retirement monthly expenses.
+      monthly_expenses = current_user.monthly_expenses.to_f
+      retirement_month = nil
+
+      current_user.months.order(:date).each do |month|
+        rate = month.interest_rate.to_f
+        # Interest is the growth part only (e.g. 1.004868 -> 0.004868)
+        monthly_interest = month.total_assets.to_f * (rate - 1.0)
+
+        if monthly_interest >= monthly_expenses
+          retirement_month = month
+          break
+        end
+      end
+
+      # Only create a retirement event if it's actually possible within the horizon.
+      if retirement_month
+        @event = Event.new(
+          name: "retirement",
+          month: retirement_month,
+          new_total_assets: retirement_month.total_assets,
+          new_saved_amount: -current_user.monthly_expenses
+        )
+      else
+        @event = nil
+      end
     end
 
-    success, @event = ApplyEvents.call(@event, current_user)
+    if @event
+      success, @event = ApplyEvents.call(@event, current_user)
+    else
+      success = true
+    end
 
     if success
 
